d071ba2d5700de709aba32f7a8877e7b2b7fc868
zbzckai <1132382311@qq.com>
2019-01-11 17:27:12 +0800

    ss

--- feature_select.py
+++ feature_select.py
@@ -267,6 +267,28 @@ df_2 = pd.DataFrame()
 df_2['pre_data'] = oof_stack
 df_2['target'] = target.values
 df_2.to_csv('shujuchakan2.csv')
+data_pre = pd.read_csv('shujuchakan2.csv')
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
+#数据数据为两列数据x和y，有表头
+df = pd.read_table('d:/LinearRegression.txt')
+#通过pandas读取为DataFrame，回归用的是矩阵数据而不是列表，数据为n个样品点和m个特征值，这里特征值只有一个因此换证nx1的矩阵
+dataSet_x = df.loc[:, 'X'].as_matrix(columns=None)
+#T为矩阵转置把1xn变成nx1
+dataSet_x = np.array([dataSet_x]).T
+dataSet_y = df.loc[:, 'Y'].as_matrix(columns=None)
+dataSet_y = np.array([dataSet_y]).T
+#regr为回归过程，fit(x,y)进行回归
+regr = LinearRegression().fit(dataSet_x, dataSet_y)
+#输出R的平方
+print(regr.score(dataSet_x, dataSet_y))
+plt.scatter(dataSet_x, dataSet_y,  color='black')
+#用predic预测，这里预测输入x对应的值，进行画线
+plt.plot(dataSet_x, regr.predict(dataSet_x), color='red', linewidth=1)
+
+
 
 pd.DataFrame()
 solution_1 = pd.read_csv('jinnan_round1_submit_20181227.csv',header=None)
